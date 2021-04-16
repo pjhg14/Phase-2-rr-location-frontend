ec0755d44c1efb8e18ea79740f832a58a4af9cfa
pj <pjhg14@gmail.com>
2021-04-16 10:15:15 -0400

    Added needed components

---
+++ src/components/main-content/UserPage.js
@@ -0,0 +1,9 @@
+function UserPage() {
+    return(
+        <div className="user-info">
+
+        </div>
+    )
+}
+
+export default UserPage 
---
+++ src/components/navigation/Nav.js
@@ -0,0 +1,9 @@
+function Nav() {
+    return(
+        <div className="navigation">
+            
+        </div>
+    )
+}
+
+export default Nav
